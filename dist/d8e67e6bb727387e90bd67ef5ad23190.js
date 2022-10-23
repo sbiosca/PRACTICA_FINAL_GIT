2d486bf8b722623aae4b85896c9bfefd57f8cdc0
Sergi Biosca <serbidaw@gmail.com>
2022-10-23 17:51:37 +0200

    Usuario1, realizando prueba hookbranch

--- dist/d8e67e6bb727387e90bd67ef5ad23190.js
+++ dist/d8e67e6bb727387e90bd67ef5ad23190.js
@@ -149,7 +149,7 @@ function Module() {
 module.bundle.Module = Module;
 
 if (!module.bundle.parent && typeof WebSocket !== 'undefined') {
-  var ws = new WebSocket('ws://localhost:61181/');
+  var ws = new WebSocket('ws://localhost:49181/');
   ws.onmessage = function(event) {
     var data = JSON.parse(event.data);
 
