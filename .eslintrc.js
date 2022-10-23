5a4ddc67531e379bdf8ed99d7ddcac65a505c871
Sergi Biosca <serbidaw@gmail.com>
2022-10-23 19:08:16 +0200

    Usuario2, realizando cambios features & release

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,20 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    mocha: true,
-    node : true
-  },
-  plugins: ['mocha'],
-  extends: 'eslint:recommended',
-  parserOptions: {
-    'ecmaVersion': 2020,
-    'sourceType': 'module'
-  },
-  rules: {
-    indent: ['error', 2],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always']
-  }
-};
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/vue3-essential"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+}
